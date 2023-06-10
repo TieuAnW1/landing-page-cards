c016d6ddd8a66ea8b49a27e3e37384c4d60f5d73
TiuAn <adwithwosso@gmail.com>
2023-06-10 15:36:13 +0700

    Set up tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-	return <div className="App">123</div>;
+	return (
+		<div className="App">
+			<h1 className="text-4xl font-bold underline">Hello world!</h1>
+		</div>
+	);
 }
 
 export default App;
